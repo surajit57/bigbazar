5cc9398aab4ccfacbca4d34066782ddd73e832f2
monster57 <surajitbarman57@yahoo.in>
2016-07-25 15:45:52 +0530

    user controller is created

---
+++ controllers/user_controller.js
@@ -0,0 +1,40 @@
+var models = require
+
+var UserController = {};
+
+Home.postSignup = (req, res, next) => {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Password must be at least 4 characters long').len(4);
+  // req.assert('phone', 'invalid phone number').isMobilePhone('en-IN');
+  var password = 
+  const errors = req.validationErrors() ;
+
+  if (errors) {
+    return next( errors );
+  }
+
+  return User.findOne({
+    where: { email: req.body.email }
+  })
+  .then( function( existingUser ){
+    if (existingUser) {
+      return Promise.reject( ApiError.create('User already exists...' ));
+    }
+  })
+  .then( function(){
+    return User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: User.generateHash(req.body.password),
+      phone: req.body.phone,
+      gender: req.body.gender
+    });
+  })
+  .then(function(user){
+    return res.apiSuccess({user:user}, 'Registration completed.');
+  })
+  .catch(next);
+};
+
+
+module.exports = UserController;
