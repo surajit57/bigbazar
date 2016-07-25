c761dd8bed892148caefe427a8425bda6deb02c6
monster57 <surajitbarman57@yahoo.in>
2016-07-25 15:46:05 +0530

    signup route is created

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var router = express.Router();
+
 var passport = require('passport');
 var _ = require('lodash');
-
+var bcrypt = require('bcrypt');
+var  UserController = require('../controllers/user_controller');
 
 var router = express.Router();
 require('../lib/passport.js')(passport);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -17,5 +21,7 @@ router.get('/login', function(req, res ){
 router.post('/login', passport.authenticate('local', {failureRedirect: '/users/login'}), function(req, res){
 	console.log('user detail: - ', req);
 });
+
+router.get('/signup' , UserController.postSignup);
 
 module.exports = router;
