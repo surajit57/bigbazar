c571d85a40e21bb1ffd87538d7ec3fb44d2c1669
monster57 <surajitbarman57@yahoo.in>
2016-07-26 19:37:05 +0530

    added mysql express session

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var session      = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var config = require( './config' );
+config.db.user = config.db.username;
+
+var sessionStore = new MySQLStore( config.db );
+
 
 var app = express();
 
@@ -26,10 +34,12 @@ app.use(cookieParser());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
+app.use(session({
+  key: config.cookieName,
+  secret: config.cookieSecret,
+  store: sessionStore,
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
