d540df45466994a42511591801dd28c3d3cee5b6
monster57 <surajitbarman57@yahoo.in>
2016-07-28 10:22:04 +0530

    refactored code

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -12,59 +12,6 @@ UserController.getSignupPage = function(req, res){
 	res.render('test.html');
 }
 
-// UserController.postSignup = (req, res, next) => {
-//   req.assert('email', 'Email is not valid').isEmail();
-//   req.assert('password', 'Password must be at least 4 characters long').len(4);
-//   req.assert('phone', 'invalid phone number').isMobilePhone('en-IN');
-//   const errors = req.validationErrors() ;
-
-//   if (errors) {
-//     return next( errors );
-//   }
-
-//   return User.findOne({
-//     where: { email: req.body.email }
-//   })
-//   .then( function( existingUser ){
-//     if (existingUser) {
-//       return Promise.reject( req.redirect('/user/signup'));
-//     }
-//   })
-//   .then( function(){
-//     return User.create({
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: User.generateHash(req.body.password),
-//       phone: req.body.phone,
-//       gender: req.body.gender
-//     });
-//   })
-//   .then(function(user){
-//     return res.render('user/login');
-//   })
-//   .catch(next);
-// };
-
-
-
-UserController.postSignup = function(req,res){
-  console.log("hello this is it");
-  var name = req.body.name;
-  var email = req.body.email;
-  var password = req.body.password;
-  var confirmPassword = req.body.confirmPassword;
-  
-    var user = User.build({
-      name: req.body.name,
-      email: req.body.email,
-      password: User.generateHash(req.body.password),
-      // phone: req.body.phone
-    });
-    user.save().then(function(user){
-      if(!user) throw Error('user has not created'); 
-      return res.redirect('/users/home')   
-    });
-};
 
 UserController.postBlog = function(req,res){
 
@@ -95,26 +42,7 @@ UserController.postBlog = function(req,res){
 UserController.getAdminSignupPage = function(req, res){
   res.render('adminPanel1/signup.html');
 }
-UserController.postAdminSignupPage = function(req,res){
-  console.log("Admin Admin");
-  var name = req.body.name;
-  var email = req.body.email;
-  // var password = randomstring.generate(7);
-  var password = req.body.password;
- console.log('body:-- ', req.body);
-    var adminuser = User.build({
-      name: req.body.name,
-      email: req.body.email,
-      password: User.generateHash(password),
-      isAdmin: 1
-      // phone: req.body.phone
-    });
-    adminuser.save().then(function(adminuser){
-      if(!adminuser) throw Error('user has not created'); 
-      console.log('user created');
-      return res.redirect('/admin/home')   
-    });
-};
+
 
 UserController.getAllBlogs = function(req, res){
     Blog.findAll({include:[{ model: models.user }]}).then(function(allblogs) {
