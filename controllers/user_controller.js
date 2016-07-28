8202b3ed0d9f769392cf022955f8d23e80b57dd4
monster57 <surajitbarman57@yahoo.in>
2016-07-28 10:27:02 +0530

    refactored code

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -18,24 +18,19 @@ UserController.postBlog = function(req,res){
   var url = req.body.blog_url;
   var UserId = req.user.id;
  
-    var blog = Blog.build({
-      url: req.body.blog_url,
-      userId: req.user.id
-    });
-    blog.save().then(function(blog){
-      if(!blog) return res.redirect('/users/home');
-          console.log('blog created');
-          User.update({
-            isBlogAdded: 1
-          },{
-            where: {
-              id: req.user.id
-            }
-          }).then(function(userData) {
-            req.flash('info', 'Blog sucessfully posted');
-            return res.redirect('/users/home') 
-          });
-    });
+  var blog = Blog.build({
+    url: req.body.blog_url,
+    userId: req.user.id
+  });
+  blog.save().then(function(blog){
+    if(!blog) return res.redirect('/users/home');
+      User.update({  isBlogAdded: 1 },
+      { where:{ id: req.user.id }})
+      .then(function(userData) {
+        req.flash('info', 'Blog sucessfully posted');
+        return res.redirect('/users/home') 
+      });
+  });
 };
 
 
@@ -49,8 +44,8 @@ UserController.getAllBlogs = function(req, res){
     res.render('adminPanel1/index.html', {blogs: allblogs});
   });
 }
-UserController.selectFor100 = function(req, res){
 
+UserController.selectFor100 = function(req, res){
   User.count({ where: { isUnder100: 1 }})
   .then(function(result) {
     if(result >= 100){
