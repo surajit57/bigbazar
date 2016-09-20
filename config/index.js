1eb21b50c3c6779e4f9965a7835823c4980b75c4
Hemant Nagarkoti <hemantnagarkoti5@gmail.com>
2016-09-20 15:33:32 +0530

    fb changes

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ var _ = require('lodash'),
   envConfig = {},
   defaultConfig = {
     appRoot: path.resolve( __dirname + '/..' ),
-    SITE_ROOT: 'http://fbbblogstar.in',
+    SITE_ROOT: 'http://localhost:3001',
     cookieName: 'xxyyzz.sid',
     cookieSecret: 'F#JKLn&()yHIO$%900nsd',
     fbLogin:{
@@ -18,7 +18,7 @@ var _ = require('lodash'),
     //   authStr: 'smtps://user%40gmail.com:pass@smtp.gmail.com',
     // },
     env: 'development',
-    port: 3000,
+    port: 3001,
   },
   config;
 
