e0c55d9fee4ec53b490406f7b9a69e7a873b2dbe
hnagarkoti <hemantnagarkoti5@gmail.com>
2016-09-26 20:06:09 +0530

    migration

--- migrations/name_field_migration.js
+++ migrations/name_field_migration.js
@@ -1,7 +1,48 @@
 
 module.exports = {
-up: function(){
+up: function(queryInterface, Sequelize){
+  return queryInterface.addColumn('users','faceBook_url',{
+    type: Sequelize.STRING,
+    allowNull: true
+  })
+  .then(function(){
+    return queryInterface.addColumn('users','twitter_url',{
+      type: Sequelize.STRING,
+      allowNull: true
+    })
+  })
+  .then(function(){
+    return queryInterface.addColumn('users','instagram_url',{
+      type: Sequelize.STRING,
+      allowNull: true
+    })
+  })
+  .then(function(){
+    return queryInterface.addColumn('users','youtube_url',{
+      type: Sequelize.STRING,
+      allowNull: true
+    })
+  })
+  .then(function(){
+    return queryInterface.addColumn('users','snapchat_url',{
+      type: Sequelize.STRING,
+      allowNull: true
+    })
+  })
 },
 down: function(){
+  return queryInterface.removColumn('users', 'faceBook_url')
+  .then(function(){
+    return queryInterface.removColumn('users', 'twitter_url')
+  })
+  .then(function(){
+    return queryInterface.removColumn('users', 'instagram_url')
+  })
+  .then(function(){
+    return queryInterface.removColumn('users', 'youtube_url')
+  })
+  .then(function(){
+    return queryInterface.removColumn('users', 'snapchat_url')
+  })
 }
 };
