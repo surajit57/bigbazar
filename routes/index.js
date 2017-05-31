588f4bfef5b71c99095c8160dc18a8e0ba1e8e60
hnagarkoti <hemantnagarkoti5@gmail.com>
2017-05-31 17:54:18 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var passport = require('passport');
 var  UserController = require('../controllers/user_controller');
 var Auth = require('../lib/auth');
 
-var routePrefix = '/home';
-
 require('../lib/passport.js')(passport);
 
 /* GET home page. */
@@ -13,32 +11,32 @@ router.get('/', function(req, res, next) {
 	// console.log('user:------------ ',req.user.id);
 
 	if(req.user){
-		res.siteRediret(routePrefix + '/users/home');
+		res.siteRediret('/users/home');
 	}
 
 	res.render('newuserLogin.html');
 });
 
-router.post(routePrefix + '/login', passport.authenticate('local-login', {failureRedirect: '/blogstar/home/users/login'}),function(req, res){
+router.post('/login', passport.authenticate('local-login', {failureRedirect: '/blogstar/home/users/login'}),function(req, res){
 	console.log('req.user.isAdmin:-- ',req.user.isAdmin);
 	if(req.user.isAdmin){
-		return res.siteRediret(routePrefix + '/admin/home');
+		return res.siteRediret('/admin/home');
 	}
-	return res.siteRediret( routePrefix + '/users/home')
+	return res.siteRediret('/users/home')
 });
 
-router.post(routePrefix + '/admin/login', passport.authenticate('local-login', {failureRedirect: '/blogstar/home/admin/login'}),function(req, res){
+router.post('/admin/login', passport.authenticate('local-login', {failureRedirect: '/blogstar/home/admin/login'}),function(req, res){
 	// if(req.user.isAdmin){
 
-		return res.siteRediret(routePrefix + '/admin/home');
+		return res.siteRediret('/admin/home');
 	// }
 	// return res.redirect('/users/home')
 });
 
-router.get(routePrefix + '/logout', function(req, res){
+router.get('/logout', function(req, res){
 	 req.logout();
 	req.flash('success', "<div class='container'>You are now logged out</div>");
-  	return res.siteRediret(routePrefix + '/users/login');
+  	return res.siteRediret('/users/login');
 })
 
 module.exports = router;
